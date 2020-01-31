28171a14316c5fdf5dc6e12a34f333a731bbbca1
Paul Methfessel <paul.methfessel@web.de>
2020-01-31 12:15:11 +0100

    Added sensors and motors

--- sketch_jan10a/sketch_jan10a.ino
+++ sketch_jan10a/sketch_jan10a.ino
@@ -11,64 +11,77 @@ int dirRight2 = 9;
 int sensor2_pin = A2;
 int sensor1_pin = A1;
 
-/* Beispielwerte
- * Schwarz:   370-380
- * Weiß:      97-161 ca. 120 im Durchschnitt
- * Grau:      187-266
- * 
- * min: 0
- * max: 900
- */
-const int min_value = 0;
 const int max_value = 900;
- 
-const int points_length = 21;
-int last_points[points_length];
+const int array_length = 120;
 
-const int bucket_length = 300;
-const int bucket_size = 3;
-int bucket_points[bucket_length] = { 0 };
+typedef struct _sensor {
+  int black_value;
+  int grey_value;
+  int white_value;
 
-int black_value = 0;
-int white_value = 0;
-int grey_value = 0;
+  int black_grey_thresh;
+  int white_grey_thresh;
 
+  int arr[array_length];
+} Sensor;
 
-int sort_desc(const void *cmp1, const void *cmp2){
-  int a = *((int *)cmp1);
-  int b = *((int *)cmp2);
-  return b - a;
-}
-
-int median_lastPoints(){
-  qsort(last_points, points_length, sizeof(last_points[0]), sort_desc);
-  return last_points[points_length/2];
-}
+Sensor s1 = Sensor(0, 0, 0, 0, 0, {0});
+Sensor s2 = Sensor(0, 0, 0, 0, 0, {0});
+Sensor s3 = Sensor(0, 0, 0, 0, 0, {0});
 
 void find_max(){
-    int third, second, first; 
-    for (int i = 0; i < bucket_length ; i ++){
-        if (bucket_points[i] > bucket_points[first]){ 
-            third = second; 
-            second = first; 
-            first = bucket_points[i]; 
-        }else if (bucket_points[i] > bucket_points[second]){ 
-            third = second; 
-            second = bucket_points[i]; 
-        }else if (bucket_points[i] > bucket_points[third]) 
-            third = bucket_points[i]; 
+    int localMax[array_length/2] = {0};
+    int pointerToCurrentLocalMax = 0;
+
+    //save all local maxima in localMax[]
+    for (int i = 1; i < array_length; i++){
+        if (arr[i-1]< arr[i] && arr[i]>arr[i+1]){
+           localMax[pointerToCurrentLocalMax] = i;
+           pointerToCurrentLocalMax ++;
+        }
     }
-    black_value = bucket_points[first];
-    grey_value = bucket_points[second];
-    white_value = bucket_points[third];
+
+    int max1 = 0, max2 = 0, max3 = 0;
+    for (int i = 0; i < array_length/2; i++){
+        if (arr[localMax[i]] > arr[max1]){
+            max3 = max2;
+            max2 = max1;
+            max1 = localMax[i];
+        }else if (arr[localMax[i]] > arr[max2]){
+            max3 = max2;
+            max2 = localMax[i];
+        }else if (arr[localMax[i]] > arr[max3]){
+            max3 = localMax[i];
+        }
+    }
+
+    black_value = std::min(max1, std::min(max2, max3));
+    white_value = std::max(max1, std::max(max2, max3));
+    grey_value = max1 + max2 + max3 - black_value - white_value;
+
+    //compute thresholds
+    black_grey_thresh = (int) ((black_value + grey_value)/ 2);
+    white_grey_thresh = (int) ((white_value + grey_value)/ 2);
 }
 
-void initialize_buckets(){
-  int point = 0;
-  for(int i = 0; i < bucket_length; i++){
-    point = analogRead(sensor2_pin);
+void addValueToBuckets(int value){
+    arr[value*array_length/max_value]++;
+}
+
+int predict(int value) {
     
-  }
+    if(num_prev_data == 1){
+        for (int i = 0; i < array_length; ++i)
+        {
+            arr[i] = 0;
+        }
+    }
+    addValueToBuckets(data[num_prev_data-1]);
+    find_max();
+    //output: 0 for black, 1 for gray and 2 for white.
+    if(value*array_length/max_value < black_grey_thresh)return 0;
+    if(value*array_length/max_value < white_grey_thresh)return 1;
+    else return 2;
 }
 
 void drive(int motor_right, int motor_left){ //values from -255 to 255
@@ -124,8 +137,6 @@ void loop() {
     last_points[i] = analogRead(sensor2_pin);
   }
   
-  Serial.print("median ");
-  Serial.println(median_lastPoints());
 }
 
 //--------------------------------------------
